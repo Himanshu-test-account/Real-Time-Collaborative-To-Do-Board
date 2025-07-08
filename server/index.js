9e441bfc179fd78cf69bf12aa43f3bcfb9a9cd77
Himanshu-test-account <143101179+Himanshu-test-account@users.noreply.github.com>
2025-07-08 22:34:51 +0530

    Add catch-all 404 handler to return JSON for unknown routes

--- server/index.js
+++ server/index.js
@@ -33,6 +33,11 @@ app.get('/', (req, res) => {
   res.json({ active: true, error: false });
 });
 
+// Catch-all 404 handler (should be after all other routes)
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found', code: 404 });
+});
+
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
